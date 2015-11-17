3956fb4bc184ab3ab796f20b398bc23ac6faabf3
eltonc88 <eltonchan@cal.berkeley.edu>
2015-11-16 21:14:48 -0800

    add three ways of add directives

--- js/index.js
+++ js/index.js
@@ -7,3 +7,38 @@ myApp.controller('UserCtrl', ['$scope', function ($scope) {
     id: Math.floor(Math.random() * 1000000)
   };
 }]);
+
+myApp.directive('customButton', function () {
+  return {
+    restrict: 'A',
+    replace: true,
+    transclude: true,
+    template: '<a href="" class="myawesomebutton" ng-transclude>' +
+                '<i class="icon-ok-sign"></i>' +
+              '</a>',
+    link: function (scope, element, attrs) {
+    }
+  };
+});
+
+myApp.directive('customButtwo', function () {
+  return {
+    restrict: 'A',
+    replace: true,
+    transclude: true,
+    templateUrl: 'js/templates/customButton.html',
+    link: function (scope, element, attrs) {
+    }
+  };
+});
+
+myApp.directive('customButhree', function () {
+  return {
+    restrict: 'A',
+    replace: true,
+    transclude: true,
+    templateUrl: 'js/templates/customButton.html',
+    link: function (scope, element, attrs) {
+    }
+  };
+});
