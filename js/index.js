ead657350a02768250f3b9f688e02ec6b87c97cd
eltonc88 <eltonchan@cal.berkeley.edu>
2015-11-17 21:01:12 -0800

    use filters

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ myApp.controller('UserCtrl', ['$scope', function ($scope) {
     username: "Todd Motto",
     id: Math.floor(Math.random() * 1000000)
   };
+  $scope.greeting = "Hello How are You?"
 }]);
 
 myApp.directive('customButton', function () {
@@ -32,13 +33,15 @@ myApp.directive('customButtwo', function () {
   };
 });
 
-myApp.directive('customButhree', function () {
-  return {
-    restrict: 'A',
-    replace: true,
-    transclude: true,
-    templateUrl: 'js/templates/customButton.html',
-    link: function (scope, element, attrs) {
+myApp.filter('reverse', function () {
+    return function (input, uppercase) {
+        var out = '';
+        for (var i = 0; i < input.length; i++) {
+            out = input.charAt(i) + out;
+        }
+        if (uppercase) {
+            out = out.toUpperCase();
+        }
+        return out;
     }
-  };
 });
